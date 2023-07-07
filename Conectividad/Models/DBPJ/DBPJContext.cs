131b8e0d846f531b9fb76506ecc4ab836851c041
Vicente Hernandez Sanchez <vicente.hernandez@poderjudicialags.gob.mx>
2023-07-07 14:30:38 -0600

    test13

--- Conectividad/Models/DBPJ/DBPJContext.cs
+++ Conectividad/Models/DBPJ/DBPJContext.cs
@@ -99,7 +99,7 @@ namespace Conectividad.Models.DBPJ
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=10.0.91.130;Database=DBPJ;Encrypt=False;persist security info=True;user=usrTemis;password=T3m1S19.;multipleactiveresultsets=True");
+                optionsBuilder.UseSqlServer("Server=172.16.20.10;Database=DBPJ;Encrypt=False;persist security info=True;user=srvlinux;password=apitest;multipleactiveresultsets=True");
             }
         }
 
